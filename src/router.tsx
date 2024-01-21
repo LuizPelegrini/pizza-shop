4d25bc5f41a56db7a4e13fe49d2777404a865688
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2024-01-21 14:59:31 +0800

    integrates router dom

---
+++ src/router.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { Dashboard } from './pages/app/dashboard'
+import { SignIn } from './pages/auth/sign-in'
+
+export const router = createBrowserRouter([
+  { path: '/', Component: Dashboard },
+  { path: '/sign-in', Component: SignIn },
+])
