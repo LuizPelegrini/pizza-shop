e1844d4d66dc24a23822cff9abd065c4ad4b9825
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2024-01-21 16:13:04 +0800

    creates signin page structure

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,10 +1,17 @@
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export const AuthLayout = () => {
   return (
-    <div>
-      <h1>Autenticacao</h1>
-      <div>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <Pizza className="size-5" />
+          <span className="font-semibold">Pizza Shop</span>
+        </div>
+        <footer>Partner Dashboard &copy; {new Date().getFullYear()}</footer>
+      </div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,32 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export const SignIn = () => {
   return (
     <>
       <Helmet title="Sign in" />
-      <h1>Dashboard</h1>
+      <div className="flex w-full max-w-[500px] flex-col gap-6 px-8">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Access Dashboard
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Follow your orders using our partner dashboard!
+          </p>
+        </div>
+        <form className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">E-mail</Label>
+            <Input id="email" type="email" />
+          </div>
+          <Button className="w-full" type="submit">
+            Access dashboard
+          </Button>
+        </form>
+      </div>
     </>
   )
 }
