b4bfc404132e7a91fb89347dbc7d99858136c0f6
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2024-01-21 15:14:57 +0800

    adds page layouts

--- src/router.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from 'react-router-dom'
-
-import { Dashboard } from './pages/app/dashboard'
-import { SignIn } from './pages/auth/sign-in'
-
-export const router = createBrowserRouter([
-  { path: '/', Component: Dashboard },
-  { path: '/sign-in', Component: SignIn },
-])
