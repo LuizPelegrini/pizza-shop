66a8de5df63fdeeb43f104c1d88c5b16d6ed9387
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2024-05-18 21:32:24 +0800

    onboard zod, axios and react query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,13 +6,17 @@ import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 
 import { router } from './router'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export function App() {
   return (
     <HelmetProvider>
       <ThemeProvider storageKey="pizzashop-theme">
         <Helmet titleTemplate="%s | Pizza Shop" />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
