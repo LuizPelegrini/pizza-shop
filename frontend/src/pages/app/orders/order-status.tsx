34df9ed53a815f70c49c6ce92e12490dddb62319
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2024-05-22 14:18:16 +0800

    refactor order status code

--- frontend/src/pages/app/orders/order-status.tsx
+++ frontend/src/pages/app/orders/order-status.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 
 type Status = "pending" | "canceled" | "processing" | "delivering" | "delivered";
 
@@ -6,23 +6,36 @@ type OrderStatusProps = {
   status: Status
 }
 
-const statusMap: Record<Status, string> = {
-  pending: 'Pending',
-  processing: 'Processing',
-  canceled: 'Canceled',
-  delivered: 'Delivered',
-  delivering: 'Delivering'
+const statusMap: Record<Status, { text: string, element: () => ReactNode }> = {
+  pending: {
+    text: 'Pending',
+    element: () => <span className="size-2 rounded-full bg-slate-400" />
+  },
+  processing: {
+    text: 'Processing',
+    element: () => <span className="size-2 rounded-full bg-amber-500" />
+  },
+  canceled: {
+    text: 'Canceled',
+    element: () => <span className="size-2 rounded-full bg-rose-500" />
+  },
+  delivered: {
+    text: 'Delivered',
+    element: () => <span className="size-2 rounded-full bg-emerald-500" />
+  },
+  delivering: {
+    text: 'Delivering',
+    element: () => <span className="size-2 rounded-full bg-amber-500" />
+  }
 }
 
 export const OrderStatus: FC<OrderStatusProps> = ({ status }) => {
+  const { text, element: StatusElement } = statusMap[status];
 
   return (
     <div className="flex items-center gap-2">
-      {status === 'pending' && <span className="size-2 rounded-full bg-slate-400" />}
-      {status === 'canceled' && <span className="size-2 rounded-full bg-rose-500" />}
-      {status === 'delivered' && <span className="size-2 rounded-full bg-emerald-500" />}
-      {['delivering', 'processing'].includes(status) && <span className="size-2 rounded-full bg-amber-500" />}
-      <span className="font-medium text-muted-foreground">{statusMap[status]}</span>
+      <StatusElement />
+      <span className="font-medium text-muted-foreground">{text}</span>
     </div>
   )
 }
