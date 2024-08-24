0f244ef2e23aec4681aeeffc86ffe2bb5abfe587
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2024-08-24 17:54:48 +0800

    redirect unauthenticated user to sign-in

--- frontend/src/pages/_layouts/app.tsx
+++ frontend/src/pages/_layouts/app.tsx
@@ -1,9 +1,33 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 import { Header } from '@/components/header'
 import { Toaster } from '@/components/ui/Toast'
+import { useEffect } from 'react';
+import { api } from '@/lib/axios';
+import { isAxiosError } from 'axios';
 
 export const AppLayout = () => {
+  const navigate = useNavigate();
+
+  // navigate the user back to sign-in if any API response returns a 401 status
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      response => response,
+      error => {
+        if(isAxiosError(error)){
+          if(error.response?.status === 401 && error.response?.data.code === 'UNAUTHORIZED') {
+            navigate('/sign-in', { replace: true })
+          }else{
+            // pass down the error, so react-query can access it, otherwise the RQ 'onSuccess' callback is called
+            throw error
+          }
+        }
+      }
+    )
+
+    return () => api.interceptors.response.eject(interceptorId)
+  }, [navigate]);
+
   return (
     <div className="flex min-h-screen flex-col antialiased">
       <Header />
