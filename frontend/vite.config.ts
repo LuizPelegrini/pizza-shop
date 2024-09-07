fbb9814f5596ce7d9d762d02fdd3d925232192bf
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2024-09-07 16:09:35 +0800

    add vitest as test runner

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,9 @@
+/// <reference types="vitest" />
 import path from 'node:path'
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,4 +11,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true
+  }
 })
